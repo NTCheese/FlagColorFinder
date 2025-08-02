62b2c1c575d56b12d6ccaca33af909ffaeef22a5
NTCheese <arirahmani2021@gmail.com>
2025-08-02 13:09:45 -0700

    gary is so disgusting me is so smeely  and ashrabeifpf

---
+++ FlagColorFinder/Form1.cs
@@ -0,0 +1,168 @@
+﻿using MySql.Data.MySqlClient;
+using MySql.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FlagColorFinder
+{
+    public partial class welcomeScreen : Form
+    {
+        public static int smallNum = 0;
+        the_ocde the_Ocde = new the_ocde();
+        SoundPlayer splat = new SoundPlayer(@"C:\Users\user\source\repos\FlagColorFinder\FlagColorFinder\lancer-splat.wav");
+        int howMany = 1;
+        int whichSortType = 0;
+        public welcomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            startButton.Visible = false;
+            alphabetical.Visible = true;
+            reverseAlphabetical.Visible = true;
+            bigSmall.Visible = true;
+            smallBig.Visible = true;
+            colorNameLabel.Visible = true;
+            colorValueLabel.Visible = true;
+            colorOrder.Visible = true;
+            reverseColorOrder.Visible = true;
+            comboBox.Visible = true;
+            smallestNumInput.Visible = true;
+            smallLabel.Visible = true;
+            // Replace "your_sound_file.wav" with the actual path to your .wav file
+            splat.Play();
+        }
+        private void welcomeScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelling(int sortType)
+        {
+            whichSortType = sortType;
+            splat.Play();
+            if (howMany == 1)
+            {
+                label = the_Ocde.Sorter(sortType);
+                colorNameLabel.Text = label[0];
+                colorValueLabel.Text = label[1];
+            }
+            if (howMany == 2)
+            {
+                label = the_Ocde.DoubleSorter(sortType);
+                colorNameLabel.Text = label[0];
+                colorValueLabel.Text = label[1];
+            }
+        }
+
+        string[] label = [""];
+        private void alphabetical_Click(object sender, EventArgs e)
+        {
+            labelling(0);
+        }
+        private void reverseAlphabetical_Click(object sender, EventArgs e)
+        {
+            labelling(1);
+        }
+
+        private void smallBig_Click(object sender, EventArgs e)
+        {
+            labelling(2);
+        }
+
+        private void bigSmall_Click(object sender, EventArgs e)
+        {
+            labelling(3);
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorOrder_Click(object sender, EventArgs e)
+        {
+            labelling(4);
+        }
+
+        private void reverseColor_Click(object sender, EventArgs e)
+        {
+            labelling(5);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            splat.Play();
+            if (comboBox.SelectedIndex == 0)
+            {
+                howMany = 1;
+                label = the_Ocde.Sorter(2);
+                colorNameLabel.Text = label[0];
+                colorValueLabel.Text = label[1];
+                vScrollBar1.Visible = false;
+            }
+            if (comboBox.SelectedIndex == 1)
+            {
+                howMany = 2;
+                label = the_Ocde.DoubleSorter(2);
+                colorNameLabel.Text = label[0];
+                colorValueLabel.Text = label[1];
+                vScrollBar1.Visible = true;
+            }
+        }
+
+        int scrollPos = 0;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (smallestNumInput.Text != null)
+                if (int.TryParse(smallestNumInput.Text, out smallNum))
+                {
+                    splat.Play();
+                    labelling(whichSortType);
+
+                    string[] numOfVals = the_Ocde.DoubleSorter(2);
+                    scrollPos = 0;
+                    vScrollBar1.Value = scrollPos;
+                    if (smallNum <= 28)
+                    {
+                        colorNameLabel.Location = new Point(274, 25);
+                        colorValueLabel.Location = new Point(395, 25);
+                        vScrollBar1.Maximum = (int)((double)(numOfVals[1].Length) * (numOfVals[1].Length) * 1/150);
+                    }
+                    else
+                    {
+                        vScrollBar1.Maximum = 0;
+                    }
+                }
+        }
+
+        int yPos = 0;
+        int factor;
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+            factor = -6;
+            yPos = colorNameLabel.Location.Y + (factor * e.NewValue - factor * e.OldValue);
+            colorNameLabel.Location = new Point(274, yPos);
+            colorValueLabel.Location = new Point(395, yPos);
+        }
+    }
+}
---
+++ FlagColorFinder/colorArrays.cs
@@ -0,0 +1,226 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlagColorFinder
+{
+    public class ColorArrays
+    {
+        static string connStr = "server=127.0.0.1;user=root;database=world;port=3306;password=TheBestServerEverWillBeStoredHere123!;";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        private string[] ColorLength()
+        {
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = $"SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE column_name != 'ID' AND table_name = 'flags'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                List<string> strings = new List<string>();
+
+                while (rdr.Read())
+                {
+                    strings.Add(rdr[0].ToString());
+                }
+
+                string[] output = new string[strings.Count];
+
+                for (int i = 0; i < output.Length; i++)
+                    output[i] = strings[i];
+
+                rdr.Close();
+                conn.Close();
+                return output;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                conn.Close();
+                return ["AAAAAAAAAAAAAGHHHHHHHHHHHHHHHHHHHH"];
+            }
+        }
+        public string Color(string color)
+        {
+            //string connStr = "server=127.0.0.1;user=root;database=world;port=3306;password=TheBestServerEverWillBeStoredHere123!;";
+            //MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = $"SELECT SUM({color}) FROM flags;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                string output = "";
+
+                while (rdr.Read())
+                {
+                    output = rdr[0].ToString();
+                }
+
+                rdr.Close();
+                conn.Close();
+                return output;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                conn.Close();
+                return $"AAAAAAAAAAAAAGHHHHHHHHHHHHHHHHHHHH";
+            }
+        }
+
+        public string[] DoubleColorNames()
+        {
+            try
+            {
+                string[] colors = ColorLength();
+                int colorLength = 0;
+                string test = "";
+
+
+                for (int i = colors.Length; i >= 0; i--)
+                {
+                    colorLength += i;
+                }
+                colorLength = colorLength - colors.Length;
+
+                int k = 0; //may have broken it change it to colorlength if borkwn
+                string[] doubleColorsFrontend = new string[colorLength];
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    for (int j = 0 + i; j < colors.Length - 1; j++)
+                    {
+                        doubleColorsFrontend[k] = colors[i] + " and " + colors[j + 1];
+                        test += colors[i] + "and" + colors[j + 1] + k + "";
+                        k++;
+                    }
+                }
+
+                //rdr.Close();
+                //conn.Close();
+                return doubleColorsFrontend;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                conn.Close();
+                return ["AAAGGHHHHH"];
+            }
+        }
+
+        public string[] DoubleColorValues()
+        {
+            try
+            {
+                string[] colors = ColorLength();
+                
+                int length = 0;
+                for (int i = colors.Length; i >= 0; i--)
+                {
+                    length += i;
+                }
+                length = length - colors.Length;
+                int[] doubleColorValues = new int[length];
+
+                int k = 0;
+                int id = 0;
+
+                Console.WriteLine("Connecting to MySQL...");
+                string sql = "";
+                MySqlCommand cmd = null;
+                MySqlDataReader rdr = null;
+
+                for (int AMONGUS = 0; AMONGUS < DoubleLength(); AMONGUS++)
+                {
+                    conn.Open();
+                    sql = $"SELECT * FROM flags WHERE ID = {id}";
+                    cmd = new MySqlCommand(sql, conn);
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        k = 0;
+                        for (int i = 0; i < colors.Length; i++)
+                        {
+                            for (int j = i + 1; j < colors.Length; j++)
+                            {
+                                if (int.Parse($"{rdr[i]} ") == 1)
+                                {
+                                    if (int.Parse($"{rdr[j]} ") == 1)
+                                    {
+                                        doubleColorValues[k]++;
+                                    }
+                                }
+                                k++;
+                            }
+                        }
+                    }
+                    id++;
+                    conn.Close();
+                    rdr.Close();
+                }
+
+                string[] output = new string[doubleColorValues.Length];
+
+                for (int AOIUSFHOASIDFHSDAPF = 0; AOIUSFHOASIDFHSDAPF < output.Length; AOIUSFHOASIDFHSDAPF++)
+                {
+                    output[AOIUSFHOASIDFHSDAPF] = doubleColorValues[AOIUSFHOASIDFHSDAPF].ToString();
+                }
+
+                rdr.Close();
+                conn.Close();
+                return output;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                conn.Close();
+                return ["AISYOHGYUKSDGBVUHYSFDIUOQFGUAGVAYISGDUFYGSDJFAJKSDFGAUSFUAHHHHHHHHHHH"];
+
+            }
+        }
+
+        public int DoubleLength()
+        {
+            //string connStr = "server=127.0.0.1;user=root;database=world;port=3306;password=TheBestServerEverWillBeStoredHere123!;";
+            //MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = $"SELECT COUNT(*) FROM flags;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                int output = 0;
+
+                while (rdr.Read())
+                {
+                    output = int.Parse(rdr[0].ToString());
+                }
+
+                rdr.Close();
+                conn.Close();
+                return output;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                conn.Close();
+                return 0000000;
+            }
+        }
+    }
+}
